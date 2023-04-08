abe3c32b6d52bc2e886deb7a37cb2edc7244a07e
TunaOfisDev <115454515+TunaOfisDev@users.noreply.github.com>
2023-04-08 19:07:09 +0300

    mynotes v3
    mynotes v3

--- mynotes/frontend/src/components/NotesPage.js
+++ mynotes/frontend/src/components/NotesPage.js
@@ -1,5 +1,22 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import {
+  Container,
+  Card,
+  CardContent,
+  Typography,
+  TextField,
+  Button,
+  Box,
+  Grid,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const NotesPage = () => {
   const [notes, setNotes] = useState([]);
@@ -41,59 +58,109 @@ const NotesPage = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Notes</h1>
-      {notes.map((note, index) => (
-        <div key={note.id}>
-          {updateIndex === index ? (
-            <>
-              <input
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-              <textarea
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
-              ></textarea>
-              <button onClick={(e) => updateNote(e, note.id)}>Update</button>
-              <button onClick={() => setUpdateIndex(null)}>Cancel</button>
-            </>
-          ) : (
-            <>
-              <h2>{note.title}</h2>
-              <p>{note.content}</p>
-              <button onClick={() => deleteNote(note.id)}>Delete</button>
-              <button
-                onClick={() => {
-                  setTitle(note.title);
-                  setContent(note.content);
-                  setUpdateIndex(index);
-                }}
-              >
-                Edit
-              </button>
-            </>
-          )}
-        </div>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="md">
+        <Typography variant="h3" gutterBottom>
+          Notes
+        </Typography>
+        <Grid container spacing={3}>
+          {notes.map((note, index) => (
+            <Grid key={note.id} item xs={12} sm={6} md={4}>
+              <Card>
+                <CardContent>
+                  {updateIndex === index ? (
+                    <>
+                      <TextField
+                        label="Title"
+                        fullWidth
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                      />
+                      <TextField
+                        label="Content"
+                        multiline
+                        rows={6}
+                        fullWidth
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                      />
+                      <Box mt={2}>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          onClick={(e) => updateNote(e, note.id)}
+                        >
+                          Update
+                        </Button>
+                        <Button
+                          color="secondary"
+                          onClick={() => setUpdateIndex(null)}
+                        >
+                          Cancel
+                        </Button>
+                      </Box>
+                    </>
+                  ) : (
+                    <>
+                      <Typography variant="h5" gutterBottom>
+                        {note.title}
+                      </Typography>
+                      <Typography variant="body1">{note.content}</Typography>
+                      <Box mt={2}>
+                        <Button
+                          variant="contained"
+                          color="secondary"
+                          onClick={() => deleteNote(note.id)}
+                        >
+                          Delete
+                        </Button>
+                        <Button
+                          color="primary"
+                                                onClick={() => {
+                        setTitle(note.title);
+                        setContent(note.content);
+                        setUpdateIndex(index);
+                      }}
+                    >
+                      Edit
+                    </Button>
+                  </Box>
+                </>
+              )}
+            </CardContent>
+          </Card>
+        </Grid>
       ))}
-      <h2>Create Note</h2>
+    </Grid>
+    <Box mt={4}>
+      <Typography variant="h4" gutterBottom>
+        Create Note
+      </Typography>
       <form onSubmit={createNote}>
-        <input
-          type="text"
-          placeholder="Title"
+        <TextField
+          label="Title"
+          fullWidth
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
-        <textarea
-          placeholder="Content"
+        <TextField
+          label="Content"
+          multiline
+          rows={6}
+          fullWidth
           value={content}
           onChange={(e) => setContent(e.target.value)}
-        ></textarea>
-        <button type="submit">Create</button>
+        />
+        <Box mt={2}>
+          <Button type="submit" variant="contained" color="primary">
+            Create
+          </Button>
+        </Box>
       </form>
-    </div>
-  );
+    </Box>
+  </Container>
+</ThemeProvider>
+);
 };
 
 export default NotesPage;
